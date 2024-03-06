56f7c3111a280113489eaac5e8c4ca0a503fea4e
22110361 <22110361@univ-brest.fr>
2024-03-06 16:11:01 +0100

    vue Lieux

--- vuejs/src/router/index.js
+++ vuejs/src/router/index.js
@@ -23,7 +23,7 @@ const routes = [
   {
     path: "/lieux",
     name: "lieux",
-    component: EvenementView,
+    component: LieuView,
   },
 ];
 
--- vuejs/src/views/LieuView.vue
+++ vuejs/src/views/LieuView.vue
@@ -3,30 +3,30 @@
     <h1>Liste des Lieux</h1>
     <table>
       <thead>
-      <tr>
-        <th>Nom</th>
-        <th>Adresse</th>
-        <th>Capacité d'accueil</th>
-        <th>Actions</th>
-      </tr>
+        <tr>
+          <th>Nom</th>
+          <th>Adresse</th>
+          <th>Capacité d'accueil</th>
+          <th>Actions</th>
+        </tr>
       </thead>
       <tbody>
-      <tr v-for="lieu in lieux" :key="lieu.id">
-        <td>{{ lieu.nom }}</td>
-        <td>{{ lieu.adresse }}</td>
-        <td>{{ lieu.capaciteAccueil }}</td>
-        <td>
-          <button @click="modifierLieu(lieu)">Modifier</button>
-          <button @click="supprimerLieu(lieu.id)">Supprimer</button>
-        </td>
-      </tr>
+        <tr v-for="lieu in lieux" :key="lieu.id">
+          <td>{{ lieu.nom }}</td>
+          <td>{{ lieu.adresse }}</td>
+          <td>{{ lieu.capaciteAccueil }}</td>
+          <td>
+            <button @click="modifierLieu(lieu)">Modifier</button>
+            <button @click="supprimerLieu(lieu.id)">Supprimer</button>
+          </td>
+        </tr>
       </tbody>
     </table>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
@@ -39,20 +39,23 @@ export default {
   },
   methods: {
     chargerLieux() {
-      axios.get('http://localhost:8080/api/v1/lieux')
-          .then(response => {
-            this.lieux = response.data;
-          })
-          .catch(error => console.error("Erreur lors du chargement des lieux", error));
-    },
-    modifierLieu(lieu) {
-      // Logique pour modifier un lieu, potentiellement ouvrir un formulaire de modification
+      axios
+        .get("http://localhost:8080/api/v1/lieux")
+        .then((response) => {
+          this.lieux = response.data;
+        })
+        .catch((error) =>
+          console.error("Erreur lors du chargement des lieux", error)
+        );
     },
     supprimerLieu(id) {
-      axios.delete(`http://localhost:8080/api/v1/lieux/${id}`)
-          .then(() => this.chargerLieux())
-          .catch(error => console.error("Erreur lors de la suppression du lieu", error));
+      axios
+        .delete(`http://localhost:8080/api/v1/lieux/${id}`)
+        .then(() => this.chargerLieux())
+        .catch((error) =>
+          console.error("Erreur lors de la suppression du lieu", error)
+        );
     },
-  }
+  },
 };
 </script>
