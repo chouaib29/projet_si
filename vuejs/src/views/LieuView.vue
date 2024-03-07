8fb9e172bd902577f87f6888b68a669685ea9cd3
chouikha <motezchouikha@yahoo.com>
2024-03-07 10:40:32 +0100

    LieuView.vue

--- vuejs/src/views/LieuView.vue
+++ vuejs/src/views/LieuView.vue
@@ -5,25 +5,25 @@
     <p id="messageLieu"></p>
     <table>
       <thead>
-      <tr>
-        <th>Nom</th>
-        <th>Adresse</th>
-        <th>Capacité d'accueil</th>
-        <th>Actions</th>
-      </tr>
+        <tr>
+          <th>Nom</th>
+          <th>Adresse</th>
+          <th>Capacité d'accueil</th>
+          <th>Actions</th>
+        </tr>
       </thead>
       <tbody>
-      <tr v-for="lieu in lieux" :key="lieu.id">
-        <td>{{ lieu.nom }}</td>
-        <td>{{ lieu.adresse }}</td>
-        <td>{{ lieu.capaciteAccueil }}</td>
-        <td>
-          <button @click="voirEvenements(lieu.id)">Voir Événements</button>
-        </td>
-        <td>
-          <button @click="supprimerLieu(lieu.id)">Supprimer</button>
-        </td>
-      </tr>
+        <tr v-for="lieu in lieux" :key="lieu.id">
+          <td>{{ lieu.nom }}</td>
+          <td>{{ lieu.adresse }}</td>
+          <td>{{ lieu.capaciteAccueil }}</td>
+          <td>
+            <button @click="voirEvenements(lieu.id)">Voir Événements</button>
+          </td>
+          <td>
+            <button @click="supprimerLieu(lieu.id)">Supprimer</button>
+          </td>
+        </tr>
       </tbody>
     </table>
   </div>
@@ -36,42 +36,42 @@ export default {
   data() {
     return {
       lieux: [],
-      url: "/api/v1/lieux/lister", // Utilisation du préfixe "/api" configuré dans vue.config.js
+      url: "/api/v1/lieux", // Utilisation du préfixe "/api" configuré dans vue.config.js
     };
   },
   methods: {
     chargerLieux() {
       axios
-          .get(this.url)
-          .then((response) => {
-            console.log(
-                "Afficher les événements pour le membre ID:",
-                response.data
-            );
-            this.setStatusEtMessage(response, "Lieu");
-            this.lieux = response.data;
-          })
-          .catch((error) => {
-            console.error("Erreur lors du chargement des lieux", error);
-          });
+        .get(this.url +"/lister")
+        .then((response) => {
+          console.log(
+            "Afficher les événements pour le membre ID:",
+            response.data
+          );
+         
+          this.lieux = response.data;
+        })
+        .catch((error) => {
+          console.error("Erreur lors du chargement des lieux", error);
+        });
     },
     supprimerLieu(id) {
       axios
-          .delete(`${this.url}/${id}`)
-          .then((response) => {
-            this.setStatusEtMessage(response, "Lieu");
-            this.chargerLieux();
-          })
-          .catch((error) => {
-            console.error("Erreur lors de la suppression du lieu", error);
-          });
+        .delete(`${this.url}/${id}`)
+        .then((response) => {
+          this.setStatusEtMessage(response, "Lieu");
+          this.chargerLieux();
+        })
+        .catch((error) => {
+          console.error("Erreur lors de la suppression du lieu", error);
+        });
     },
     setStatusEtMessage(response, type) {
       document.getElementById(`status${type}`).innerHTML =
-          "Status : " +
-          (response.status >= 200 && response.status < 300 ? "OK" : "KO");
+        "Status : " +
+        (response.status >= 200 && response.status < 300 ? "OK" : "KO");
       document.getElementById(`message${type}`).innerHTML =
-          "Message : " + response.status;
+        "Message : " + response.status;
     },
   },
   mounted() {
